d94ca68bacc9e7152028eb9d27bb48ee4835b612
helooworldfor3 <81641175+helooworldfor3@users.noreply.github.com>
2023-08-15 22:34:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git eqosplus  https://github.com/sirpdboy/sirpdboy-package/tree/main/luci-app-eqosplus' >>feeds.conf.default
+echo 'src-git eqosplus  https://github.com/sirpdboy/luci-app-eqosplus' >>feeds.conf.default
